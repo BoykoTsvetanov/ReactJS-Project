80d002ba9c0ee98555b3026506ddf4bf6ccc3836
Boyko <81263767+BoykoTsvetanov@users.noreply.github.com>
2021-03-27 23:07:40 +0200

    second commit

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            TRVL
+          </Link>
+          <div className="menu-icon" onClick={handleClick}></div>
+          <ul className={"nav-menu active"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className="nav-links">
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/products" className="nav-links">
+                Products
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/sign-up" className="nav-links-mobile">
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {<Button buttonStyle="btn--outline">SIGN UP</Button>}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
